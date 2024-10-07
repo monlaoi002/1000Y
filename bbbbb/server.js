f2f87bd4efa4afa3b027082383a9d128414c6fac
monlaoi002 <94620492+monlaoi002@users.noreply.github.com>
2024-10-08 05:17:38 +0900

    b

--- bbbbb/server.js
+++ bbbbb/server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '../bbbbb')));
 
 // 기본 라우트
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'default.html'));
 });
 
 // 서버 시작
