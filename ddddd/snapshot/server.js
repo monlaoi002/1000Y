78baefa16f705bcccebe42ca5a2dd1fca41c1232
monlaoi002 <94620492+monlaoi002@users.noreply.github.com>
2024-10-08 02:38:19 +0900

    Create server.js

---
+++ ddddd/snapshot/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'ddddd')));
+
+// 기본 라우트
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// 서버 시작
+app.listen(PORT, () => {
+  console.log(`Server is running on http://54.180.152.111:${PORT}`);
+});
